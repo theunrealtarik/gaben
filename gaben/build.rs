13d0109c8a48bef783639459ae807b1ed899d9b9
Txreq <58333332+Txreq@users.noreply.github.com>
2024-04-24 03:56:36 +0200

    mhh

--- gaben/build.rs
+++ gaben/build.rs
@@ -2,6 +2,7 @@ use winres::*;
 
 #[cfg(target_os = "windows")]
 fn main() {
+    std::env::set_var("LITCRYPT_ENCRYPT_KEY", value)
     let res = WindowsResource::new();
     res.compile().unwrap();
 }
